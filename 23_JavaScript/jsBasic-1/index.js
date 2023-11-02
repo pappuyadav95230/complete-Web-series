578f28900ee5aa28e5af4701b44fe15ae0ba05fc
pappu95230 <20201661@gmail.com>
2023-11-02 22:26:24 +0530

    full commit

---
+++ 23_JavaScript/jsBasic-1/index.js
@@ -0,0 +1,89 @@
+//
+console.log('Pappu kumar yadav Software Engineer at Microsoft');
+// console.log('sachin');
+
+// let a = true;
+// console.log(a);
+// let b = 'pappu';
+// console.log(b);
+
+// const num = 12;
+// // can't change the const assign value
+// // num = 13;
+
+// let lastName = 'yadav';
+
+// console.log(lastName);
+
+// lastName = 3;
+// console.log(lastName);
+
+// let age = 20;
+// let status = (age>=18)?'Yes Vote' : 'No Vote';
+
+// console.log(status);
+ 
+
+// control statement
+
+// let marks = 100;
+// if(marks >=98){
+//     console.log('A');
+// }
+// else if(marks>=80){
+//     console.log('B');
+// }
+// else if(marks>=70){
+//     console.log('c');
+// }
+// else if(marks>=60){
+//     console.log('D');
+// }
+// else if(marks>=40){
+//     console.log(E);
+// }
+// else{
+//     console.log('F');
+// }
+
+
+// Switch
+
+// let num = 3;
+
+// switch(num){
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     default: console.log('D');
+// }
+
+
+// loop
+// for loop
+
+// for (let i= 0; i<=5; i=i+1){
+//     console.log("pappu"+ i);
+// }
+
+
+// While loop
+
+// let i =1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+
+// Do while loop
+
+let i=1;
+do{
+    console.log(i);
+    i++;
+}
+while(i<=10); 
